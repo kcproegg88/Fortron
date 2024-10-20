b248edf61c466d9b63f6608ae5fb7b793c4d0d9f
krishna <krischauhan01@gmail.com>
2024-10-19 20:36:24 -0400

    somewhat working

--- DCM/main.py
+++ DCM/main.py
@@ -1,18 +1,29 @@
 from PyQt5.QtWidgets import QApplication, QLabel, QWidget, QMainWindow, QLineEdit, QPushButton
 from PyQt5.QtWidgets import QFormLayout, QHBoxLayout, QVBoxLayout
+from PyQt5.QtGui import QPixmap
+from PyQt5.QtCore import Qt
 import sys
 
 
 class DCM(QMainWindow):
     def __init__(self):
         super().__init__()
+        self.logo = QPixmap("logo.png")
         self.setWindowTitle('DCM')
-        # self.setStyleSheet("background-color: rgb(0, 0, 0);")
-        self.welcome_screen()
+        # self.setStyleSheet("background-color: rgb(255, 255, 255);")
+        self.run_gui()
+
+    def run_gui(self):
+        container = QWidget()
+        container.setLayout(self.welcome_screen())
+        self.setCentralWidget(container)
 
     def welcome_screen(self):
         welcome_layout = QHBoxLayout()
-        QLabel("Login")
+
+        #login page side
+        leftside = QVBoxLayout()
+
         login_layout = QFormLayout()
         button1 = QLabel("User Name")
         lineEdit1 = QLineEdit()
@@ -20,20 +31,34 @@ class DCM(QMainWindow):
         lineEdit2 = QLineEdit()
         login_layout.addRow(button1, lineEdit1)
         login_layout.addRow(button2, lineEdit2)
-        welcome_layout.addLayout(login_layout)
-        self.textbox = QLineEdit(self)
-        welcome_layout.addWidget(self.textbox)
 
-        self.textbox1 = QLineEdit(self)
-        self.textbox1.resize(300, 40)
-        welcome_layout.addWidget(self.textbox1)
-        welcome_layout.addWidget(QPushButton("Left-Most"))
-        welcome_layout.addWidget(QPushButton("Center"), 1)
-        welcome_layout.addWidget(QPushButton("Right-Most"), 2)
+        leftside.addStretch()
+        leftside.addLayout(login_layout)
+        leftside.addStretch()
 
-        container = QWidget()
-        container.setLayout(welcome_layout)
-        self.setCentralWidget(container)
+        #
+        rightside = QVBoxLayout()
+        logolable = QLabel()
+        logolable.setPixmap(self.logo)
+
+        rightside.addStretch()
+        rightside.addWidget(logolable, alignment=Qt.AlignCenter)
+        rightside.addStretch()
+
+        welcome_layout.addLayout(leftside, 1)
+        welcome_layout.addLayout(rightside, 1)
+        # textbox = QLineEdit(self)
+        # welcome_layout.addWidget(textbox)
+        # self.textbox1 = QLineEdit(self)
+        # self.textbox1.resize(300, 40)
+        # welcome_layout.addWidget(self.textbox1)
+        # welcome_layout.addWidget(QPushButton("Left-Most"))
+        # welcome_layout.addWidget(QPushButton("Center"), 1)
+        # welcome_layout.addWidget(QPushButton("Right-Most"), 2)
+        return welcome_layout
+        # container = QWidget()
+        # container.setLayout(welcome_layout)
+        # self.setCentralWidget(container)
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
