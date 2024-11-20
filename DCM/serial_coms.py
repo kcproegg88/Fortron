ae87f162ce73fcd803972a7dcca5d553d6b75b42
krishna <krischauhan01@gmail.com>
2024-11-20 15:17:29 -0500

    testing serial code

---
+++ DCM/serial_coms.py
@@ -0,0 +1,10 @@
+import serial
+
+import serial
+
+ser = serial.Serial(port='COM3', baudrate=9600, timeout=1)
+
+data = [i for i in range(20)]
+byte_data = bytes(data)
+ser.write(byte_data)
+ser.close()
