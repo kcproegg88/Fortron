3f06dda1b04f71b79ce880753ebae8e2d537e8c4
Hamza Alfalo <hmzalf@gmail.com>
2024-11-20 16:10:55 -0500

    correct test serial py file

--- DCM/serial_coms.py
+++ DCM/serial_coms.py
@@ -2,9 +2,14 @@ import serial
 
 import serial
 
-ser = serial.Serial(port='COM5', baudrate=9600, timeout=1)
+ser = serial.Serial(port='COM5', baudrate=115200, timeout=1)
 
 data = [i for i in range(20)]
+data[5] = 0
+data[0] = 0
+data[19] = 85
+data[18] = 22 
+print("period")
 byte_data = bytes(data)
 ser.write(byte_data)
 ser.close()
