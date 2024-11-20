ac2d998ec5fdbfad006f318dbb92b12446cd2b45
Hamza Alfalo <hmzalf@gmail.com>
2024-11-20 15:37:33 -0500

    modified serial coms

--- DCM/serial_coms.py
+++ DCM/serial_coms.py
@@ -2,7 +2,7 @@ import serial
 
 import serial
 
-ser = serial.Serial(port='COM3', baudrate=9600, timeout=1)
+ser = serial.Serial(port='COM5', baudrate=9600, timeout=1)
 
 data = [i for i in range(20)]
 byte_data = bytes(data)
