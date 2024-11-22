71a885ef566ceb61df374d4c96f277eead621a4b
krishna <krischauhan01@gmail.com>
2024-11-22 16:57:34 -0500

    testing recieve

--- DCM/test.py
+++ DCM/test.py
@@ -1,29 +1,40 @@
+import serial
 import serial.tools.list_ports
+import time
 
 
-def get_connected_ports():
+def check_connection(ser_num):
     # Retrieve a list of all available ports
     ports = serial.tools.list_ports.comports()
 
     if not ports:
         print("No COM ports found.")
-        return []
+        return 0
 
-    connected_ports = []
     for port in ports:
-        port_name = port.device
-        description = port.description
-        hwid = port.hwid
-        connected_ports.append((port_name, description, hwid))
+        if not port.hwid:
+            continue
+        print(f"Port: {port.device}, Description: {port.description}, HWID: {port.hwid}")
+        if f"SER={ser_num}" in port.hwid.split():
+            return port.device
 
-    return connected_ports
+    print("Device Not Found")
+    return 0
 
 
-# Example usage
-connected_ports = get_connected_ports()
-if connected_ports:
-    print("Connected COM Ports:")
-    for port_name, description, hwid in connected_ports:
-        print(f"Port: {port_name}, Description: {description}, HWID: {hwid}")
-else:
-    print("No serial ports are currently connected.")
+def serial_receive():
+    port = check_connection("000621000000")
+    if port == 0:
+        return 0
+    with serial.Serial(port=port, baudrate=115200, timeout=1) as ser:
+        while True:
+            if ser.in_waiting > 0:  # Check if data is available to read
+                data = ser.readline()  # Read a line of data
+                print("Received:", data.decode('utf-8').strip())  # Print received data
+
+            time.sleep(0.1)  # Small delay to prevent excessive CPU
+
+try:
+    serial_receive()
+except KeyboardInterrupt:
+    print("Program interrupted")
