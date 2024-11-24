951722bff6d1e91a5820187613460d6e4b6df7ae
krishna <krischauhan01@gmail.com>
2024-11-23 20:10:06 -0500

    some changes

--- DCM/main.py
+++ DCM/main.py
@@ -4,6 +4,7 @@ import sys
 import os
 from login_registration import LoginPage, RegisterPage
 from mainpage import MainPage
+from parameter_display import PaceMakerMode
 from serial_coms import serial_stuff
 from PyQt5.QtCore import QTimer
 
@@ -17,7 +18,6 @@ class DCM(QMainWindow):  # Main application window
         self.logo = QPixmap(os.path.join(current_dir, "logo.png"))
         self.user_file = os.path.join(current_dir, "users.txt")
         self.logo = QPixmap("logo.png")
-        self.user_file = "users.txt"
         self.users, self.data = {}, {}
         self.read_users()
 
@@ -26,14 +26,8 @@ class DCM(QMainWindow):  # Main application window
 
         # State variables for page navigation and user data
         self.max_users, self.key = 10, "1234"
-        self.pacemaker_modes, self.user = {}, ""
-        self.default_data = {"AOO": [60, 120, 5, 40], "VOO": [60, 120, 5, 40],
-                             "AAI": [60, 120, 5, 40, 75, 250, 250, 60, 0], "VVI": [60, 120, 5, 40, 250, 320, 60, 0],
-                             "AOOR": [60, 120, 120, 5, 40, 30, 8, 5], "VOOR": [60, 120, 120, 5, 40, 30, 8, 5],
-                             "AAIR": [60, 120, 120, 5, 40, 75, 250, 250, 60, 0, 30, 8, 5], 
-                             "VVIR": [60, 120, 120, 5, 40, 250, 320, 60, 0, 30, 8, 5]}
+        self.init_modes()
 
-        self.user_data = {mode: values[:] for mode, values in self.default_data.items()}
         self.page = 0
         self.pages_stacked_widget = QStackedWidget()
         self.login_page, self.register_page, self.main_page = LoginPage(self), RegisterPage(self), MainPage(self)
@@ -42,6 +36,52 @@ class DCM(QMainWindow):  # Main application window
         self.run_gui()
         #serial_stuff(self)
         #QTimer.singleShot(1, QApplication.quit)
+
+
+    def init_modes(self):
+        """Creates and adds parameter widgets for each pacing mode."""
+        self.pacemaker_modes, self.user = {}, ""
+        self.default_data = {"AOO": [60, 120, 5, 40], "VOO": [60, 120, 5, 40],
+                             "AAI": [60, 120, 5, 40, 75, 250, 250, 60, 0], "VVI": [60, 120, 5, 40, 250, 320, 60, 0],
+                             "AOOR": [60, 120, 120, 5, 40, 30, 8, 5], "VOOR": [60, 120, 120, 5, 40, 30, 8, 5],
+                             "AAIR": [60, 120, 120, 5, 40, 75, 250, 250, 60, 0, 30, 8, 5],
+                             "VVIR": [60, 120, 120, 5, 40, 250, 320, 60, 0, 30, 8, 5]}
+
+        self.user_data = {mode: values[:] for mode, values in self.default_data.items()}
+        modes_params = {
+            "AOO": [["Lower rate limit", 30, 175, 1], ["Upper rate limit", 50, 175, 1],
+                    ["Atrial amplitude", 5, 32, 10], ["Atrial pulse width", 5, 190, 100]],
+            "VOO": [["Lower rate limit", 30, 175, 1], ["Upper rate limit", 50, 175, 1],
+                    ["Ventricular amplitude", 5, 32, 10], ["Ventricular pulse width", 5, 190, 100]],
+            "AAI": [["Lower rate limit", 30, 175, 1], ["Upper rate limit", 50, 175, 1],
+                    ["Atrial amplitude", 5, 32, 10], ["Atrial pulse width", 5, 190, 100],
+                    ["Atrial sensitivity", 25, 1000, 100], ["ARP", 150, 500, 1], ["PVARP", 150, 500, 1],
+                    ["Hysteresis", 30, 175, 1], ["Rate Smoothing", 0, 25, 1]],
+            "VVI": [["Lower rate limit", 30, 175, 1], ["Upper rate limit", 50, 175, 1],
+                    ["Ventricular amplitude", 5, 32, 10], ["Ventricular pulse width", 5, 190, 100],
+                    ["Ventricular sensitivity", 25, 1000, 100], ["VRP", 150, 500, 1],
+                    ["Hysteresis", 30, 175, 1], ["Rate Smoothing", 0, 25, 1]],
+            "AOOR": [["Lower rate limit", 30, 175, 1], ["Upper rate limit", 50, 175, 1],
+                     ["Maximum Sensor Rate", 50, 175, 1], ["Atrial amplitude", 5, 32, 10],
+                     ["Atrial pulse width", 5, 190, 100],
+                     ["Reaction Time", 10, 50, 1], ["Response Factor", 1, 16, 1], ["Recovery Time", 2, 16, 1]],
+            "VOOR": [["Lower rate limit", 30, 175, 1], ["Upper rate limit", 50, 175, 1],
+                     ["Maximum Sensor Rate", 50, 175, 1], ["Ventricular amplitude", 5, 32, 10],
+                     ["Ventricular pulse width", 5, 190, 100],
+                     ["Reaction Time", 10, 50, 1], ["Response Factor", 1, 16, 1], ["Recovery Time", 2, 16, 1]],
+            "AAIR": [["Lower rate limit", 30, 175, 1], ["Upper rate limit", 50, 175, 1],
+                     ["Maximum Sensor Rate", 50, 175, 1], ["Atrial amplitude", 5, 32, 10],
+                     ["Atrial pulse width", 5, 190, 100], ["Atrial Sensitivity", 25, 1000, 100], ["ARP", 150, 500, 10],
+                     ["PVARP", 150, 500, 10], ["Hysteresis", 30, 175, 1], ["Rate Smoothing", 0, 25, 1],
+                     ["Reaction Time", 10, 50, 10], ["Response Factor", 1, 16, 1], ["Recovery Time", 2, 16, 1]],
+            "VVIR": [["Lower rate limit", 30, 175, 1], ["Upper rate limit", 50, 175, 1],
+                     ["Maximum Sensor Rate", 50, 175, 1], ["Ventricular amplitude", 5, 32, 10],
+                     ["Ventricular pulse width", 5, 190, 100], ["Ventricular Sensitivity", 25, 1000, 100], ["VRP", 150, 500, 10],
+                     ["Hysteresis", 30, 175, 1], ["Rate Smoothing", 0, 25, 1],
+                     ["Reaction Time", 10, 50, 10], ["Response Factor", 1, 16, 1], ["Recovery Time", 2, 16, 1]]}
+
+        for mode, params in modes_params.items():
+            self.pacemaker_modes[mode] = PaceMakerMode(mode, self, params)
 
     def read_users(self):
         """Load user credentials and settings from file."""
@@ -62,11 +102,6 @@ class DCM(QMainWindow):  # Main application window
         """Save the current user's settings for all modes."""
         data = [" ".join(map(str, self.pacemaker_modes[i].send_values())) for i in self.pacemaker_modes]
         self.write_user(self.user, self.users[self.user], list(map(str, data)))
-
-    def sign_out(self):
-        """Log out current user and return to the login page."""
-        self.page = 0
-        self.run_gui()
 
     def update_modes(self):
         for mode in self.pacemaker_modes:
--- DCM/mainpage.py
+++ DCM/mainpage.py
@@ -1,7 +1,6 @@
 from PyQt5.QtWidgets import QLabel, QWidget, QPushButton, QStackedWidget
 from PyQt5.QtWidgets import QHBoxLayout, QVBoxLayout, QComboBox
 from PyQt5.QtCore import Qt
-from parameter_display import PaceMakerMode
 
 
 class MainPage(QWidget):
@@ -12,53 +11,12 @@ class MainPage(QWidget):
         self.params_stacked_widget = QStackedWidget()  # Holds mode-specific parameter interfaces
 
         # Initialize modes with their respective parameters
-        self.init_modes()
+        for mode in self.dcm.pacemaker_modes:
+            self.params_stacked_widget.addWidget(self.dcm.pacemaker_modes[mode])
 
         # Main layout setup
-        self.layout = QVBoxLayout()
         self.setup_layout()
         self.setLayout(self.layout)
-        # Connect mode selection combo box to update parameters
-        self.mode_combo.currentTextChanged.connect(self.update_mode_selection)
-
-    def init_modes(self):
-        """Creates and adds parameter widgets for each pacing mode."""
-        modes_params = {
-            "AOO": [["Lower rate limit", 30, 175, 1], ["Upper rate limit", 50, 175, 1],
-                    ["Atrial amplitude", 5, 32, 10], ["Atrial pulse width", 5, 190, 100]],
-            "VOO": [["Lower rate limit", 30, 175, 1], ["Upper rate limit", 50, 175, 1],
-                    ["Ventricular amplitude", 5, 32, 10], ["Ventricular pulse width", 5, 190, 100]],
-            "AAI": [["Lower rate limit", 30, 175, 1], ["Upper rate limit", 50, 175, 1],
-                    ["Atrial amplitude", 5, 32, 10], ["Atrial pulse width", 5, 190, 100],
-                    ["Atrial sensitivity", 25, 1000, 100], ["ARP", 150, 500, 1], ["PVARP", 150, 500, 1],
-                    ["Hysteresis", 30, 175, 1], ["Rate Smoothing", 0, 25, 1]],
-            "VVI": [["Lower rate limit", 30, 175, 1], ["Upper rate limit", 50, 175, 1],
-                    ["Ventricular amplitude", 5, 32, 10], ["Ventricular pulse width", 5, 190, 100],
-                    ["Ventricular sensitivity", 25, 1000, 100], ["VRP", 150, 500, 1],
-                    ["Hysteresis", 30, 175, 1], ["Rate Smoothing", 0, 25, 1]],
-            "AOOR": [["Lower rate limit", 30, 175, 1], ["Upper rate limit", 50, 175, 1],
-                     ["Maximum Sensor Rate", 50, 175, 1], ["Atrial amplitude", 5, 32, 10],
-                     ["Atrial pulse width", 5, 190, 100],
-                     ["Reaction Time", 10, 50, 1], ["Response Factor", 1, 16, 1], ["Recovery Time", 2, 16, 1]],
-            "VOOR": [["Lower rate limit", 30, 175, 1], ["Upper rate limit", 50, 175, 1],    
-                     ["Maximum Sensor Rate", 50, 175, 1], ["Ventricular amplitude", 5, 32, 10],
-                     ["Ventricular pulse width", 5, 190, 100], 
-                     ["Reaction Time", 10, 50, 1], ["Response Factor", 1, 16, 1], ["Recovery Time", 2, 16, 1]],
-            "AAIR": [["Lower rate limit", 30, 175, 1], ["Upper rate limit", 50, 175, 1],
-                     ["Maximum Sensor Rate", 50, 175, 1], ["Atrial amplitude", 5, 32, 10],
-                     ["Atrial pulse width", 5, 190, 100], ["Atrial Sensitivity", 25, 1000, 100], ["ARP", 150, 500, 10],
-                     ["PVARP", 150, 500, 10], ["Hysteresis", 30, 175, 1], ["Rate Smoothing", 0, 25, 1],
-                     ["Reaction Time", 10, 50, 10], ["Response Factor", 1, 16, 1], ["Recovery Time", 2, 16, 1]],
-            "VVIR": [["Lower rate limit", 30, 175, 1], ["Upper rate limit", 50, 175, 1],
-                     ["Maximum Sensor Rate", 50, 175, 1], ["Ventricular amplitude", 5, 32, 10],
-                     ["Ventricular pulse width", 5, 190, 100], ["Ventricular Sensitivity", 25, 1000, 100], ["VRP", 150, 500, 10],
-                     ["Hysteresis", 30, 175, 1], ["Rate Smoothing", 0, 25, 1],
-                     ["Reaction Time", 10, 50, 10], ["Response Factor", 1, 16, 1], ["Recovery Time", 2, 16, 1]]}
-
-        for mode, params in modes_params.items():
-            mode_widget = PaceMakerMode(mode, self.dcm, params)
-            self.params_stacked_widget.addWidget(mode_widget)
-            self.dcm.pacemaker_modes[mode] = mode_widget
 
     def setup_layout(self):
         """Sets up the main interface layout with labels, mode selection, and status indicators."""
@@ -105,8 +63,9 @@ class MainPage(QWidget):
         """
         mode_label = QLabel("Select Pacing Mode:")
         self.mode_combo = QComboBox()
-        self.mode_combo.addItems(["AOO", "VOO", "AAI", "VVI","AOOR", "VOOR", "AAIR", "VVIR"])
+        self.mode_combo.addItems(self.dcm.pacemaker_modes.keys())
         self.mode_combo.setStyleSheet(combo_style)
+        self.mode_combo.currentTextChanged.connect(self.update_mode_selection)
         
 
         # Parameter display
@@ -117,10 +76,13 @@ class MainPage(QWidget):
         params_layout.addWidget(self.params_stacked_widget)
         self.params_widget.setLayout(params_layout)
 
+        # Interface Bare with Save Load and Send button
+
         # Bottom bar with status and sign-out button
         bottom_bar = self.setup_bottom_bar()
 
         # Assemble layout
+        self.layout = QVBoxLayout()
         self.layout.addWidget(title)
         self.layout.addWidget(mode_label)
         self.layout.addWidget(self.mode_combo)
@@ -155,13 +117,16 @@ class MainPage(QWidget):
         # Sign Out Button
         sign_out_button = QPushButton("Sign Out")
         sign_out_button.setStyleSheet("background-color: #0275d8; color: white; padding: 5px; font-size: 14px; border-radius: 5px;")
-        sign_out_button.clicked.connect(self.dcm.sign_out)
+        sign_out_button.clicked.connect(self.sign_out)
         bottom_bar.addWidget(sign_out_button)
         return bottom_bar
 
     def update_mode_selection(self):
         """Switches displayed parameters based on selected pacing mode."""
-        selected_mode = self.mode_combo.currentText()
-        index = self.mode_combo.findText(selected_mode)
-        if index != -1:
-            self.params_stacked_widget.setCurrentIndex(index)
+        index = self.mode_combo.findText(self.mode_combo.currentText())
+        self.params_stacked_widget.setCurrentIndex(index)
+
+    def sign_out(self):
+        """Log out current user and return to the login page."""
+        self.dcm.page = 0
+        self.dcm.run_gui()
